b3b61725996a0917b081903507f4161c7800bdfc
VDani3 <villacarmona.dani@gmail.com>
2023-11-13 10:17:59 +0100

    petit canvi

---
+++ client_flutter/lib/widgets/result_painter.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/cupertino.dart';
+
+class ResultPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
